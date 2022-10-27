1ce3200aef75f8320fa5277ef8aa36eb5d8e3112
slivskist <slivskist@gmail.com>
2022-10-27 22:46:03 +0300

    Added assignment 8

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,29 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator() {    
+};
+
+Calculator.prototype.sum = function(a, b) {
+    console.log(a + b);
+};
+
+Calculator.prototype.subtraction = function(a, b) {
+    console.log(a - b);
+};
+
+Calculator.prototype.multiplication = function(a, b) {
+    console.log(a * b);
+};
+
+Calculator.prototype.division = function(a, b) {
+    console.log(a / b);
+};
+
+const calc1 = new Calculator();
+
+calc1.sum(4, 2);
+calc1.subtraction(10, 6);
+calc1.multiplication(7, 3);
+calc1.division(3, 3);
