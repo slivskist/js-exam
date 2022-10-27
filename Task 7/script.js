93c162ba8fe2b6b26e50f0ed95562f30f9f3ebe7
slivskist <slivskist@gmail.com>
2022-10-27 16:35:26 +0300

    Added assignment 7

--- Task 7/script.js
+++ Task 7/script.js
@@ -10,3 +10,11 @@ const audi = {
   year: 2005,
   color: 'white',
 };
+
+const showObjectKeys = (object) => {
+  console.log(Object.keys(object));
+}
+
+showObjectKeys(audi);
+
+// Atrodo per daug paprasta užduotis. Tikiuosi, kad teisingai supratau.
